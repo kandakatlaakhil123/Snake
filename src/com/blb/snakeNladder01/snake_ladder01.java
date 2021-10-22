af0eac4c22bf00f21da55c630d0a827b57abde50
kandakatlaakhil123 <91108116+kandakatlaakhil123@users.noreply.github.com>
2021-10-22 15:50:43 +0530

    added: U_C1 snakeladder

--- src/com/blb/snakeNladder01/snake_ladder01.java
+++ src/com/blb/snakeNladder01/snake_ladder01.java
@@ -5,5 +5,9 @@ import java.sql.SQLOutput;
 public class snake_ladder01 {
     public static void main(String[] args) {
         System.out.println("welcome for snake ladder problem");
+
+                int position = 0;
+                System.out.println("Position of the player = " + position);
+            }
     }
-}
+
