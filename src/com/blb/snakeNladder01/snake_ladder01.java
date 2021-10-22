7fa52421648deca795b024c8baf520a11b2a9f78
kandakatlaakhil123 <91108116+kandakatlaakhil123@users.noreply.github.com>
2021-10-22 15:39:19 +0530

    added: Akhil

---
+++ src/com/blb/snakeNladder01/snake_ladder01.java
@@ -0,0 +1,9 @@
+package com.blb.snakeNladder01;
+
+import java.sql.SQLOutput;
+
+public class snake_ladder01 {
+    public static void main(String[] args) {
+        System.out.println("welcome for snake ladder problem");
+    }
+}
